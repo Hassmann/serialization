b8bd55147fa68a50eb226505ce909d5e0dccb72d
Andreas Hassmann <andreas@hassmann.com>
2023-06-14 23:29:41 +0200

    ...

---
+++ Test/TypeTests.cs
@@ -0,0 +1,32 @@
+﻿using SLD.Serialization;
+using System.Text;
+
+namespace Test
+{
+    public class TypeTests
+    {
+        [Fact]
+        public void SerializeDateTime()
+        {
+            var original = DateTime.Now;
+
+            var stream = new MemoryStream();
+
+            using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
+            {
+                writer.Write(original);
+            }
+
+            stream.Position = 0;
+
+            DateTime deserialized;
+
+            using (var reader = new BinaryReader(stream, Encoding.UTF8))
+            {
+                deserialized = reader.ReadDateTime();
+            }
+
+            Assert.Equal(original, deserialized);
+        }
+    }
+}
