f2ecfda879ec3d694b783e001050535515a40e5b
Hassmann <andreas@hassmann.com>
2024-01-28 14:33:17 +0100

    base64 no trailing '='

--- Serialization.Hash/Hash.cs
+++ Serialization.Hash/Hash.cs
@@ -1,56 +1,53 @@
 ﻿using System;
 using System.IO;
 
-namespace SLD.Serialization
+namespace SLD.Serialization;
+
+using XXHash3NET;
+
+public struct Hash : IBinarySerializable
 {
-    using XXHash3NET;
+    public const int BitLength = 64;
+    public const int Size = BitLength / 8;
+    private readonly ulong _value;
 
-    public struct Hash : IBinarySerializable
+    public static Hash Empty = From(new byte[0]);
+
+    public Hash(ulong value)
     {
-        public const int BitLength = 64;
-        public const int Size = BitLength / 8;
-        private readonly ulong _value;
-
-        public static Hash Empty = From(new byte[0]);
-
-        public Hash(ulong value)
-        {
-            _value = value;
-        }
-
-        public Hash(BinaryReader reader)
-        {
-            _value = reader.ReadUInt64();
-        }
-
-        public ulong Value64
-            => _value;
-
-        public byte[] Bytes
-            => BitConverter.GetBytes(_value);
-
-        public static Hash From(ReadOnlySpan<byte> data)
-        {
-            ulong xx = XXHash3.Hash64(data);
-
-            return new Hash(xx);
-        }
-
-        public static Hash Deserialize(string serializedHash)
-        {
-            byte[] bytes = Convert.FromBase64String(serializedHash);
-
-            return new Hash(BitConverter.ToUInt64(bytes, 0));
-        }
-
-        public void Serialize(BinaryWriter writer)
-        {
-            writer.Write(Value64);
-        }
-
-        public override string ToString()
-        {
-            return Convert.ToBase64String(Bytes);
-        }
+        _value = value;
     }
+
+    public Hash(BinaryReader reader)
+    {
+        _value = reader.ReadUInt64();
+    }
+
+    public ulong Value64
+        => _value;
+
+    public byte[] Bytes
+        => BitConverter.GetBytes(_value);
+
+    public static Hash From(ReadOnlySpan<byte> data)
+    {
+        ulong xx = XXHash3.Hash64(data);
+
+        return new Hash(xx);
+    }
+
+    public static Hash Deserialize(string serializedHash)
+    {
+        byte[] bytes = Convert.FromBase64String(serializedHash);
+
+        return new Hash(BitConverter.ToUInt64(bytes, 0));
+    }
+
+    public void Serialize(BinaryWriter writer)
+    {
+        writer.Write(Value64);
+    }
+
+    public override string ToString()
+        => Convert.ToBase64String(Bytes, Base64FormattingOptions.None);
 }
